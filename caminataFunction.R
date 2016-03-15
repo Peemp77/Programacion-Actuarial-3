6f5a12e3c91d0aadc9881ce0991a281427b7a038
Pablo Hernandez <pa_sc21@outlook.com>
2016-03-15 15:47:18 -0600

    CaminataFunction
    Se creó una función a partir de la práctica anterior

---
+++ caminataFunction.R
@@ -0,0 +1,28 @@
+CaminataAleatoria <- function(y,x){
+    
+    faa <- c()
+    
+    for(i in 1:x){
+        faa <- c(faa,y)
+        v <- runif(1,0,1)
+        
+        if ( v <0.5 ) {
+            moneda <- 1
+        }  else {
+            moneda <-0
+        }
+        
+        
+        if (moneda == 1) {
+            y <- y - 1
+        } else {
+            y <- y + 1
+        }  
+    }
+faa
+}
+
+CaminataAleatoria(0,10)
+    
+    
+    
